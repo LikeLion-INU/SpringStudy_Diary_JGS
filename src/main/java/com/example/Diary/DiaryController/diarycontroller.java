5257313af2756f7dfef86013706f24e4d52e2eaa
gisu1102 <gisu1102@gmail.com>
2024-04-14 15:30:01 +0900

    mod

---
+++ src/main/java/com/example/Diary/DiaryController/diarycontroller.java
@@ -0,0 +1,13 @@
+package com.example.Diary.DiaryController;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class diarycontroller {
+    @GetMapping("/get")
+    public String getTest(){
+        return "success";
+    }
+}
