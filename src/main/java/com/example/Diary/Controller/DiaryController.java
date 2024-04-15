ae6fd7af5fb4c7b1b6a0f02f9cd9fe047088ec04
jg <ella.code.j@gmail.com>
2024-04-15 13:44:19 +0900

    Mod : 클래스명은 대문자로 시작해야함.

--- src/main/java/com/example/Diary/Controller/DiaryController.java
+++ src/main/java/com/example/Diary/Controller/DiaryController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class diarycontroller {
+public class DiaryController {
     @GetMapping("/get")
     public String getTest(){
         return "success";
